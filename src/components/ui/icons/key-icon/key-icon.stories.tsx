e482d25114abbadd855db957a55e804d0fdb23f4
Sofia Klimova <klimovasonya@gmail.com>
2023-04-22 20:22:29 +0300

    add icons as components

---
+++ src/components/ui/icons/key-icon/key-icon.stories.tsx
@@ -0,0 +1,8 @@
+import { KeyIcon } from "./key-icon";
+
+export default {
+    component: KeyIcon,
+    title: 'Icons/Key Icon',
+};
+
+export const Default = () => <KeyIcon/>
---
+++ src/components/ui/icons/location-on-map-icon/location-on-map-icon.stories.tsx
@@ -0,0 +1,8 @@
+import { LocationOnMapIcon } from "./location-on-map-icon";
+
+export default {
+    component: LocationOnMapIcon,
+    title: 'Icons/Location on Map Icon',
+};
+
+export const Default = () => <LocationOnMapIcon/>
