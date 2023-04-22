c1f6ac327b251bf45c9bd9dc46542d8959892df5
AlexanderSub <subbotin_aleks@mail.ru>
2023-04-22 20:37:55 +0300

    nav link button

--- src/components/side-navigation/SideNavigation.tsx
+++ src/components/side-navigation/SideNavigation.tsx
@@ -1,33 +1,16 @@
+import SideNavLink from '../ui/SideNavLink/SideNavLink';
 import './SideNavigation.scss'
-import { Link } from "react-router-dom";
 
 export default function SideNavigation() {
+
   return (
     <nav className="nav">
-      <Link to={'/map'} className='nav_link'>
-        <div className='nav_link_image nav_link_image_map'/>
-        <p className='nav_link_text'>Карта заявок</p>
-      </Link>
-      <Link to={'/map'} className='nav_link'>
-        <div className='nav_link_image nav_link_image_active'/>
-        <p className='nav_link_text'>Активные заявки</p>
-      </Link>
-      <Link to={'/map'} className='nav_link'>
-        <div className='nav_link_image nav_link_image_completed'/>
-        <p className='nav_link_text'>Завершённые заявки</p>
-      </Link>
-      <Link to={'/map'} className='nav_link'>
-        <div className='nav_link_image nav_link_image_approve'/>
-        <p className='nav_link_text'>Подтверждение / Блокировка</p>
-      </Link>
-      <Link to={'/map'} className='nav_link'>
-        <div className='nav_link_image nav_link_image_statistics'/>
-        <p className='nav_link_text'>Статистика</p>
-      </Link>
-      <Link to={'/map'} className='nav_link'>
-        <div className='nav_link_image nav_link_image_create'/>
-        <p className='nav_link_text'>Создание / Редактирование заявки</p>
-      </Link>
+      <SideNavLink path='/map' type='map' visible />
+      <SideNavLink path='/active' type='active' visible />
+      <SideNavLink path='/completed' type='completed' visible />
+      <SideNavLink path='/approve' type='approve' visible />
+      <SideNavLink path='/statistics' type='statistics' visible />
+      <SideNavLink path='/create' type='create' visible />
     </nav>
   )
 }
---
+++ src/components/ui/SideNavLink/SideNavLink.tsx
@@ -0,0 +1,49 @@
+import './SideNavLink.scss'
+import { Link } from "react-router-dom"
+
+interface ISideNavLink {
+  path: string
+  type: 'map' | 'active' | 'completed' | 'approve' | 'statistics' | 'create'
+  visible: boolean
+}
+
+export default function SideNavLink({path, type, visible}: ISideNavLink) {
+
+  let text = ''
+
+  switch (type) {
+    case 'map':
+      text = 'Карта заявок'
+      break;
+    case 'active':
+      text = 'Активные заявки'
+      break;
+    case 'completed':
+      text = 'Завершённые заявки'
+      break;
+    case 'approve':
+      text = 'Подтверждение / Блокировка'
+      break;
+    case 'statistics':
+      text = 'Статистика'
+      break;
+    case 'create':
+      text = 'Создание / Редактирование заявки'
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <>
+      {visible && 
+      <div>
+        <Link to={path} className='nav_link'>
+          <div className={`nav_link_image nav_link_image_${type}`}/>
+          <p className='nav_link_text'>{text}</p>
+        </Link>
+      </div>
+      }
+    </>
+  )
+} 
