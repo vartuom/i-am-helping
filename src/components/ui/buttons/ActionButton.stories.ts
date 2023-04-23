4badb71bc29e7a113fd184424cc8e4d8fc838649
Elena Nikiforova <vizualnoe@gmail.com>
2023-04-23 02:20:29 +0200

    working on button component with storybook

---
+++ src/components/ui/buttons/ActionButton.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ActionButton } from './ActionButton';
+
+const meta = {
+  title: 'Buttons/ActionButton',
+  component: ActionButton,
+  tags: ['autodocs'],
+} satisfies Meta<typeof ActionButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const TextButtonDark: Story = {
+  args: {
+    variant: "text",
+    color: "dark",
+    label: "Применить",
+    type: "submit"
+  },
+};
+
+export const TextButtonLight: Story = {
+  args: {
+    variant: "text",
+    color: "light",
+    label: "Заблокировать",
+    type: "button"
+  },
+};
+
+export const IconButton: Story = {
+  args: {
+    variant: "icon",
+    size: "small",
+    color: "dark",
+    type: "button",
+    icon: true,
+  },
+};
