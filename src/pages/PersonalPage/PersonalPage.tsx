ef3997a92290cf10ee0eabad9e71a9d4962ff31d
Yulia Lushinskaya <crimson-alloya@yandex.ru>
2023-04-24 14:33:49 +0700

    Avatar
    Простой компонент для вставки аватаров разных размеров туда, куда надо (карточки, чаты, меню, и т.д.)

--- src/pages/PersonalPage/PersonalPage.tsx
+++ src/pages/PersonalPage/PersonalPage.tsx
@@ -1,3 +1,4 @@
+import { Avatar } from '../../components/avatar/avatar.component'
 import NewApplication from '../../components/new-application/NewApplication'
 import './PersonalPage.scss'
 
@@ -6,6 +7,7 @@ const PersonalPage = () => {
   return (
     <main className="main">
       <section className='personalInfo'>Личные данные</section>
+      <Avatar url='https://fraguru.com/mdimg/avatariru/m.298472.jpg' size={160}/>
       <section className='applicationsInfo'>
         <div className='personalNav'>Personal Navigation</div>
         <div className='newApplication'>
