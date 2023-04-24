9837be8c4d83b34311cc2a358bba2096ba848417
VadimLitau <vadimlitau@yandex.ru>
2023-04-24 20:37:53 +0600

    input stories

--- src/components/ui/Input/Input.stories.tsx
+++ src/components/ui/Input/Input.stories.tsx
@@ -1,1 +1,25 @@
-import React from 'react'
+import type {Meta, StoryObj} from '@storybook/react';
+import Input from './Input';
+
+const meta = {
+  title: 'Components/Input',
+    component: Input
+} as Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Name: Story = {
+    args:
+    {
+      typeInput:'name'
+    }
+};
+
+export const Phone: Story = {
+  args:
+  {
+    typeInput:'number'
+  }
+};
