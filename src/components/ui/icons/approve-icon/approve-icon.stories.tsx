38bc727fd60a7b48ac9dcea5e266f31b08c2ea3a
ob <oberezhinskiy@gmail.com>
2023-04-26 19:58:20 +0300

    refactored icons

---
+++ src/components/ui/icons/approve-icon/approve-icon.stories.tsx
@@ -0,0 +1,8 @@
+import { ApproveIcon } from "./approve-icon";
+
+export default {
+    component: ApproveIcon,
+    title: 'Icons/Approve Icon',
+};
+
+export const Default = () => <ApproveIcon type='blue' />
