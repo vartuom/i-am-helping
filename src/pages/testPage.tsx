1dab8609daad41b97c1b64cae12ac975e6799f8d
vartuom <v.artuom@ya.ru>
2023-04-29 00:46:43 +0800

    modals/routing

--- src/pages/testPage.tsx
+++ src/pages/testPage.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import {NavLink, useLocation, useNavigate} from "react-router-dom";
 
 const TestPage = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
     return (
         <>
-        <div>
-            Привет, я тут из Роута :) Ссылки делаются <NavLink to="/anotherTestPage"> так</NavLink>.
-        </div>
-        <button>Волонтёр</button>
-        <button>Реципиент</button>
-        <button>Администратор</button>
-        <button>Главный администратор</button>
+            <div>
+                Привет, я тут из Роута :) Ссылки делаются <NavLink to="/anotherTestPage"> так</NavLink>.
+            </div>
+            <button>Волонтёр</button>
+            <button>Реципиент</button>
+            <button>Администратор</button>
+            <button>Главный администратор</button>
+            <button
+                type="button"
+                onClick={() =>
+                    navigate("modalOne", {
+                        state: { background: location },
+                    })
+                }
+            >
+                Модальное окно 1
+            </button>
         </>
     );
 };
