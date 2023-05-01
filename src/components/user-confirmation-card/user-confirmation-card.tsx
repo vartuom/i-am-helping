ff1d6025b70ea32a74c734d145f18ca72b3d6209
Elena Nikiforova <vizualnoe@gmail.com>
2023-05-02 00:50:51 +0200

    added web layout

--- src/components/user-confirmation-card/user-confirmation-card.tsx
+++ src/components/user-confirmation-card/user-confirmation-card.tsx
@@ -1,15 +1,16 @@
 import styles from './user-confirmation-card.module.scss';
 import { FC } from 'react';
-import Avatar from '../../images/avatar-admin-small.png';
+//import Avatar from '../../images/avatar-admin-small.png';
 import { Button } from '../ui/buttons/Button';
 import { BallsIcon } from '../ui/icons/icons';
 import { FinishedApplicationIcon } from '../ui/icons/icons';
 import { KeyIcon } from '../ui/icons/icons';
+import { Avatar } from '../avatar/Avatar';
 
 export const user: TUser = {
-  id: 112233,
-  name: 'Иванов Иван Иванович',
-  phone: '+7(000)000-00-00',
+  id: 11111114,
+  name: 'Петров Петр Петрович',
+  phone: '+7(000)000-00-04',
   about: 'Я люблю музыку, книги и кошек Я люблю музыку, книги и кошек Я люблю музыку, книги и кошек',
   image: `${Avatar}`,
   balls: 2500,
@@ -33,22 +34,35 @@ interface TUser {
 
 const UserConfirmationCard: FC<TUser> = (user: TUser) => {
   return (
-    <article className={styles.card}> 
-      <img className={styles.avatar} src={user.image} alt="фото пользователя"/>
+    <li className={styles.card}> 
+      <div className={styles.avatar}>
+        <Avatar size={62} url={"https://fraguru.com/mdimg/avatariru/m.298472.jpg"} />
+      </div>
+
       <div className={styles.info}>
 
-        {/* 
-        
-        <div className={styles.about}>
+        {/* <h3 className={styles.about}>
           <p className={styles.aboutName}>{user.name}</p>
           <p className={styles.aboutId}>ID {user.id}</p>
           <div className={styles.aboutDescription}>
             <p className={styles.aboutMe}>Тел.:</p>
             <p className={styles.aboutContact}>{user.phone}</p>
-            <p className={styles.aboutMe}>О себе:</p>
-            <p className={styles.aboutContact}>{user.about}</p>
           </div>
-        </div>
+        </h3> */}
+
+        <div className={styles.about}>
+          <h3 className={styles.aboutName}>Петров Петр Петрович</h3>
+          <p className={styles.aboutId}>ID 11111114</p>
+          <div className={styles.aboutDescription}>
+            <p className={styles.aboutMe}>Тел.:</p>
+            <p className={styles.aboutContact}>+7(000) 000-00-04</p>
+          </div>
+          <div className={styles.temp}></div> 
+        </div>  
+
+            
+
+        {/* 
         {user.status === 'recipient' ? 
         (<>
         <div className={styles.progress}>
@@ -79,9 +93,33 @@ const UserConfirmationCard: FC<TUser> = (user: TUser) => {
         </div>
         */}
 
+        <div className={styles.buttons}>
+          <Button
+            label="Подтвердить"
+            onClick={() => {}}
+            theme="dark"
+            type="submit"
+            variant="text"
+          />
+          <Button
+            label="Заблокировать"
+            onClick={() => {}}
+            theme="light"
+            type="button"
+            variant="text"
+          />
+          <Button
+            label="Дать ключи"
+            onClick={() => {}}
+            theme="light"
+            type="button"
+            variant="text"
+          />
+        </div>
+
         
       </div>
-    </article>
+    </li>
   );
 };  
 
