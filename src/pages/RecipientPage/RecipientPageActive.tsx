2815cab2a3ca050d2b53c898bbcc061c46b1ffc2
Sofia Klimova <klimovasonya@gmail.com>
2023-05-04 18:33:14 +0300

    fix components

--- src/pages/RecipientPage/RecipientPageActive.tsx
+++ src/pages/RecipientPage/RecipientPageActive.tsx
@@ -31,7 +31,48 @@ export const RecipientPageActive = () => {
       status: 'recipient',
   }
 
-  const [requests, setRequests] = useState<TRequest[]>([
+  const [requests, setRequests] = useState<TRequest[]>([{
+    id: 1,
+    category: "категория",
+    date: "24.10.2022",
+    time: "16:00",
+    address: "ул. Потолочного д. 9",
+    title: "Заголовок",
+    content: "Описание заявки с возможностью развернуть и прочитать Описание заявки с возможностью развернуть и прочитать Описание заявки с  прочитать Описание заявки с ",
+    bulls: 3,
+    person_img: "https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty00.jpg",
+    person_name: "Петров Петр Петрович",
+    phone: "+7(000) 000-00-00",
+
+  },
+  {
+    id: 2,
+    category: "категория",
+    date: "24.10.2022",
+    time: "16:00",
+    address: "ул. Потолочного д. 9",
+    title: "Заголовок",
+    content: "Описание заявки с возможностью развернуть и прочитать Описание заявки с возможностью развернуть и прочитать Описание заявки с  прочитать Описание заявки с ",
+    bulls: 3,
+    person_img: "https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty00.jpg",
+    person_name: "Петров Петр Петрович",
+    phone: "+7(000) 000-00-00",
+
+  },
+  {
+    id: 3,
+    category: "категория",
+    date: "24.10.2022",
+    time: "16:00",
+    address: "ул. Потолочного д. 9",
+    title: "Заголовок",
+    content: "Описание заявки с возможностью развернуть и прочитать Описание заявки с возможностью развернуть и прочитать Описание заявки с  прочитать Описание заявки с ",
+    bulls: 3,
+    person_img: "https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty00.jpg",
+    person_name: "Петров Петр Петрович",
+    phone: "+7(000) 000-00-00",
+
+  },
   ]);
 
   const avatarParams: IAvatarProps = {
